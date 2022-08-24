599c8c9c4f65ce6039069e616f8d87edc089f639
mollikacomputer <mollikacomputer3@gmail.com>
2022-08-24 23:54:21 +0600

    dashboard add service field ui design

--- src/Components/Dashboard/AddService.js
+++ src/Components/Dashboard/AddService.js
@@ -3,8 +3,15 @@ import React from 'react';
 const AddService = () => {
     return (
         <div>
-            <h2 className='text-5xl' > Add Service</h2>
-            
+            <h2 className='text-5xl my-6' > Add Service</h2>
+            <form className='flex flex-col' >
+            <input type="text" placeholder="Service Name" class="input input-bordered w-full max-w-xs mb-2" /> 
+            <input type="text" placeholder="image link" class="input input-bordered w-full max-w-xs mb-2" /> 
+            <input type="number" placeholder="Your Budget" class="input input-bordered w-full max-w-xs mb-2" /> 
+            <textarea placeholder="short description" class="textarea textarea-bordered w-full max-w-xs mb-2"></textarea>
+            <input type='submit' value='submit' class="btn w-full max-w-xs" />
+            </form>
+
         </div>
     );
 };
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div class="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col items-center ">
+            <div class="drawer-content flex flex-col">
                 {/* <!-- Page content here --> */}
                 <Outlet/>
                 <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
