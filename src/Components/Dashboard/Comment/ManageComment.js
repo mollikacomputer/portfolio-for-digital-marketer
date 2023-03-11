de6f5e4b0467b5a546e8618c5df00955ca686fc5
mollikacomputer <mollikacomputer3@gmail.com>
2023-03-11 23:34:26 +0600

    some update feature solved

--- src/Components/Dashboard/Comment/ManageComment.js
+++ src/Components/Dashboard/Comment/ManageComment.js
@@ -1,13 +1,69 @@
-import React from 'react';
-import ManageCommentData from './ManageCommentData';
+import React, { useEffect, useState } from "react";
+import ManageCommentData from "./ManageCommentData";
 
 const ManageComment = () => {
-    return (
-        <div>
-            <h2> Manage Comment</h2>
-            <ManageCommentData/>
-        </div>
-    );
+
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+      fetch(`http://localhost:5000/comment`)
+      .then( res => res.json())
+      .then(data => setComments(data))
+  } ,[]);
+  
+  const handleDeleteComment = (id) =>{
+    const proceed = window.confirm("Are you sure you want to delete?");
+    if(proceed){
+      // console.log("deleting service data with id", id);
+      const url = `http://localhost:5000/comment/${id}`;
+      fetch(url, {
+        method: 'DELETE'
+      })
+      .then(res=> res.json())
+      .then(data => {
+        if(data.deletedCount>0){
+          console.log('deleted');
+          const remaining = comments.filter(comment => comment?._id !== id );
+          setComments(remaining);
+        }
+        console.log(data);
+      })
+    }
+  };
+  // handle update
+  return (
+    <div>
+      
+      <h2 className="text-2xl "> Manage  {comments.length} Comment </h2>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th> Comment Name </th>
+              <th> Picture </th>
+              <th> Delete </th>
+              <th> Update </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              comments.map( comment => 
+              <ManageCommentData 
+                key={comment?._id} 
+                comment={comment} 
+                handleDeleteComment={handleDeleteComment}
+                comments={comments}
+                setComments={setComments}
+                />
+                  )
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageComment;
+
+
--- src/Components/Dashboard/Comment/ManageCommentData.js
+++ src/Components/Dashboard/Comment/ManageCommentData.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { HiTrash, HiPencilAlt } from "react-icons/hi";
+import { Link } from 'react-router-dom';
 
-const ManageCommentData = () => {
+const ManageCommentData = ({comment, comments, setComments, handleDeleteComment}) => {
+
+    console.log("Manage service data", comment);
     return (
         <div>
-            <h2 className='text-2xl'> Manage Comment Data</h2> 
+      <tr>
+        <td>
+          <div class="flex items-center space-x-3">
+            <div class="avatar">
+              <div class="mask mask-squircle w-12 h-12">
+                <img src={comment?.image} alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+          </div>
+        </td>
+        <td>
+          {comment?.name}
+         </td>
+        <td> { comment?.budget}</td>
+        <td>
+          <button onClick={()=>handleDeleteComment(comment._id)} className="btn btn-ghost btn-xs"> <HiTrash/> </button>
+        </td>
+        <td>
+         <Link to={`/dashboard/updatecomment/${comment._id}`} > <button className="btn btn-ghost btn-xs"> <HiPencilAlt/> </button> </Link>
+        </td>
+      </tr>
         </div>
     );
 };
 
 export default ManageCommentData;
+// import React from 'react';
+
+// const ManageCommentData = () => {
+//     return (
+//         <div>
+//             <h2 className='text-2xl'> Manage Comment Data</h2> 
+//         </div>
+//     );
+// };
+
+// export default ManageCommentData;
