255078be2b2c1300938708388c211ba8096951c7
mollikacomputer <mollikacomputer3@gmail.com>
2023-04-07 09:39:41 +0600

    environment variable is updated

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,18 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyArb94UnDRDI7F_ur_ZeJvSopqlr1ldfa8",
-  // authDomain: "walid-689ed.firebaseapp.com",
-  // projectId: "walid-689ed",
-  // storageBucket: "walid-689ed.appspot.com",
-  // messagingSenderId: "692715049247",
-  // appId: "1:692715049247:web:8262f7c613663efa20a02a"
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyArb94UnDRDI7F_ur_ZeJvSopqlr1ldfa8",
+  authDomain: "walid-689ed.firebaseapp.com",
+  projectId: "walid-689ed",
+  storageBucket: "walid-689ed.appspot.com",
+  messagingSenderId: "692715049247",
+  appId: "1:692715049247:web:8262f7c613663efa20a02a"
+  // apiKey: process.env.REACT_APP_apiKey,
+  // authDomain: process.env.REACT_APP_authDomain,
+  // projectId: process.env.REACT_APP_projectId,
+  // storageBucket: process.env.REACT_APP_storageBucket,
+  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  // appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
